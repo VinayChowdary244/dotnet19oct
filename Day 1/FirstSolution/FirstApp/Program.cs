9315e0f04b20be7e3bdc96f54928af1f7faf2c68
Vinay Penumaka <vinaychowdary244@gmail.com>
2023-10-19 17:11:47 +0530

    First Commit

---
+++ Day 1/FirstSolution/FirstApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Concurrent;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static int GetARandomNumber()
+        {
+            int num1 = new Random().Next(1, 200);
+            return num1;
+        }
+        static void CheckNumberStatus()
+        {
+            int num1;
+            
+            num1 = GetARandomNumber();
+            if (num1 > 100)
+                Console.WriteLine("Too big. The numebr is " + num1);
+            else
+                Console.WriteLine($"The number {num1} is Okay");
+        }
+
+        static void AddTwoNums()
+        {
+            int FirstNum= GetARandomNumber();
+            int SecondNum= GetARandomNumber();
+            int Total = FirstNum + SecondNum;
+            Console.WriteLine("the sum is : " + Total);
+        }
+        static void Main(string[] args)
+        {
+            //string name;
+            //Console.WriteLine("Enter your name");
+            //name = Console.ReadLine();
+            //Console.WriteLine("welcome " + name);
+            //Console.WriteLine($"welcome {name} ");
+            //int num1;
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            CheckNumberStatus();
+            AddTwoNums();
+            
+        }
+    }
+}
