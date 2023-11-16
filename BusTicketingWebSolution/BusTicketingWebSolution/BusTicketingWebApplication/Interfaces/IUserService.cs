a0465d86ee0f3fe0dd182f0a5916527d5563763c
Vinay Penumaka <84056806+VinayChowdary244@users.noreply.github.com>
2023-11-16 14:07:48 +0530

    Add files via upload

---
+++ BusTicketingWebSolution/BusTicketingWebSolution/BusTicketingWebApplication/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using BusTicketingWebApplication.Models.DTOs;
+
+namespace BusTicketingWebApplication.Interfaces
+{
+    public interface IUserService
+    {
+        UserDTO Login(UserDTO userDTO);
+        UserDTO Register(UserDTO userDTO);
+    }
+}
---
+++ BusTicketingWebSolution/BusTicketingWebSolution/BusTicketingWebApplication/Services/BusService.cs
@@ -0,0 +1,38 @@
+﻿using BusTicketingWebApplication.Exceptions;
+using BusTicketingWebApplication.Interfaces;
+using BusTicketingWebApplication.Exceptions;
+using BusTicketingWebApplication.Interfaces;
+using BusTicketingWebApplication.Models;
+using BusModelLibrary;
+
+namespace BusTicketingWebApplication.Services
+{
+    public class BusService : IBusService
+    {
+        private readonly IRepository<int, Bus> _busRepository;
+
+        public BusService(IRepository<int, Bus> repository)
+        {
+            _busRepository = repository;
+        }
+        public Bus Add(Bus bus)
+        {
+            if (bus.Capacity > 10)
+            {
+                var result = _busRepository.Add(bus);
+                return result;
+            }
+            return null;
+        }
+
+        public List<Bus> GetBuses()
+        {
+            var buses = _busRepository.GetAll();
+            if (buses != null)
+            {
+                return buses.ToList();
+            }
+            throw new NoBusesAvailableException();
+        }
+    }
+}
